799a80b825e5d065d09980ec967582e20d6876d7
wzh <2840831011@qq.com>
2022-03-12 00:21:47 +0800

    0.34

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,5 +1,4 @@
 #!bin/bash
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
-touch $3
-cat $1|grep $2>$3
+grep -n $2 $1 | awk -F: '{print $1}' > $3
