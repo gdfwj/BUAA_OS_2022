d47c2cb7fbb645909ed511f6480a185bad802e35
wzh <2840831011@qq.com>
2022-03-17 14:15:26 +0800

    2.1

--- lab0-exam.sh
+++ lab0-exam.sh
@@ -5,4 +5,4 @@ mkdir hello_os_dir
 cp os_hello ./hello_os_dir/os_hello
 cp os_hello ./hello_os_dir/hello_os
 rm os_hello
-grep -i "os_hello" hello_os.c > hello_os.txt
+grep -i -n "os_hello" hello_os.c > hello_os.txt
