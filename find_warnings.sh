12e2a308524a2fa2c0eef4b34c971b01a3271654
wzh <2840831011@qq.com>
2022-03-17 14:35:53 +0800

    ex1.1

--- find_warnings.sh
+++ find_warnings.sh
@@ -3,11 +3,13 @@ gcc -Wall $1 -o test 2> warning.txt
 grep "warning" warning.txt > result.txt
 sed -i 's/warning: //g' result.txt
 a=1
-if [$? -eq 0]
+b=0
+if [$? -eq $b]
 then
 while [$a -le $2]
 do
 $a | ./test >> result.txt
+a=$(expr $a+1)
 done
 fi
 pwd
