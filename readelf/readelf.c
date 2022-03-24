c3467a7831ac54ae41e6fcfc41603df4ebb452cd
wzh <2840831011@qq.com>
2022-03-24 15:08:53 +0800

    1.1ee

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -68,18 +68,36 @@ int readelf(u_char *binary, int size)
 		sh_entry_size = ehdr->e_phentsize;
 		//shdr=(Elf32_Shdr*)ptr_sh_table;
         // for each section header, output section number and section addr.
-		Elf32_Half lastend=0;
+		Elf32_Half start[10], end[10];
 		for(Nr = 0; Nr < sh_entry_count; Nr++){
 			shdr = (Elf32_Phdr*)(ptr_sh_table+Nr*sh_entry_size);
-			if((shdr->p_vaddr<lastend) && Nr!=0){
-				printf("Conflict at page va : 0x%x\n", lastend & 0x1000);
+			start[Nr] = shdr->p_vaddr;
+			end[Nr] = shdr->p_memsz;
+		}
+		Elf32_Half temp;
+		int i,j;
+		for(i=0;i<Nr;i++){
+			for(j=0;j<Nr-i-2;j++){
+				if(start[j]>start[j+1]){
+					temp = start[j];
+					start[j] = start[j+1];
+					start[j+1] = temp;
+					temp = end[j];
+					end[j] = end[j+1];
+					end[j+1] = temp;
+				}
+			}
+		}
+		temp = end[0];
+		for(i=1;i<Nr;i++){
+			if(start[i]<temp){
+				printf("(Conflict at page va : 0x%x\n", temp & 0x1000);
 				return 0;
 			}
-			else if((shdr->p_vaddr<((lastend&0x100)+0x100)) && Nr!=0){
-				printf("Overlay at page va : 0x%x\n", lastend & 0x100);
+			else if(start[i]<((temp&0x1000)+0x1000)){
+				printf("Overlay at page va : 0x%x\n", temp & 0x1000);
 				return 0;
 			}
-			lastend = shdr-> p_memsz + shdr->p_vaddr;
 		}
 			for (Nr = 0; Nr < sh_entry_count; Nr++) {
 				shdr = (Elf32_Phdr*)(ptr_sh_table+Nr*sh_entry_size);
