395d5d0ffa3cb5f92b0562ab84439e468351ce25
wzh <2840831011@qq.com>
2022-04-14 17:05:12 +0800

    sth

--- init/init.c
+++ init/init.c
@@ -8,23 +8,16 @@ static void buddy_test(){
 u_int pa[10];
 u_char pi;
 int i;
-for(i = 0;i <= 9;i++){
-buddy_alloc(4096 * (1 << i), &pa[i], &pi);
-printf("%x %d\n", pa[i], (int)pi);
-}
-for(i = 0;i <= 9;i += 2) buddy_free(pa[i]);
-for(i = 0;i <= 9;i += 2){
-buddy_alloc(4096 * (1 << i) + 1, &pa[i], &pi);
-printf("%x %d\n", pa[i], (int)pi);
-}
-for(i = 1;i <= 9;i += 2) buddy_free(pa[i]);
-for(i = 1;i <= 9;i += 2){
-buddy_alloc(4096 * (1 << i) + 1, &pa[i], &pi);
-printf("%x %d\n", pa[i], (int)pi);
-}
-for(i = 0;i <= 9;i++) buddy_free(pa[i]);
-printf("%d\n", buddy_alloc(4096 * 1024, &pa[0], &pi));
 printf("%d\n", buddy_alloc(4096 * 1024 + 1, &pa[0], &pi));
+buddy_alloc(2048 * 1024, &pa[0], &pi);
+printf("%x %d\n", pa[0], (int)pi);
+for(i = 2;i <= 8;i++){
+buddy_alloc(4096 * 1024, &pa[i], &pi);
+printf("%x %d\n", pa[i], (int)pi);
+}
+buddy_free(pa[0]);
+printf("%d", buddy_alloc(4096 * 1024, &pa[0], &pi));
+printf("%x %d\n", pa[0], (int)pi);
 }
 void mips_init(){
 mips_detect_memory();
