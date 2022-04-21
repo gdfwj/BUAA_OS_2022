85138e294e67aa7fd567491b000bb4d57fd54696
wzh <2840831011@qq.com>
2022-04-21 15:55:02 +0800

    sth

--- mm/pmap.c
+++ mm/pmap.c
@@ -148,8 +148,8 @@ int inverted_page_lookup(Pde *pgdir, struct Page *pp, int vpn_buffer[]){
 	int count=0;
 	struct Page *temppage;
 	Pte *pgtable_entry;
-	Pte *pte;
-	for(va=0;va<0x800000000;va+=0x1000){
+	//Pte *pte;
+	for(va=0;va<0x80000000;va+=0x1000){
 		//pgdir_walk(pgdir, va, 0, &pte);
 		//if ((*pte & PTE_V) == 0) {
 		//	continue;    //the page is not in memory.
