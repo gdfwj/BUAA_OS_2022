caa469d44472020119fc290087ac962dcd7576e8
wzh <2840831011@qq.com>
2022-04-21 15:59:12 +0800

    sth

--- mm/pmap.c
+++ mm/pmap.c
@@ -148,14 +148,17 @@ int inverted_page_lookup(Pde *pgdir, struct Page *pp, int vpn_buffer[]){
 	int count=0;
 	struct Page *temppage;
 	Pte *pgtable_entry;
-	//Pte *pte;
+	Pte *pte;
 	for(va=0;va<0x80000000;va+=0x1000){
-		//pgdir_walk(pgdir, va, 0, &pte);
-		//if ((*pte & PTE_V) == 0) {
-		//	continue;    //the page is not in memory.
-		//}
-		temppage = page_lookup(pgdir, va, &pgtable_entry);
-		if(temppage==pp){
+		pgdir_walk(pgdir, va, 0, &pte);
+		if (pte == 0) {
+			continue;
+		}
+		if ((*pte & PTE_V) == 0) {
+			continue;    //the page is not in memory.
+		}
+		//temppage = page_lookup(pgdir, va, &pgtable_entry);
+		if(pa2page(*pte)==pp){
 			vpn_buffer[count]=PPN(va);
 			count++;
 		}
