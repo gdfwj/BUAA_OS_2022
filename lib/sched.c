9771bc5fd6947ff1dab976bc8ba97678c166a852
wzh <2840831011@qq.com>
2022-05-08 01:00:18 +0800

    sth

--- lib/sched.c
+++ lib/sched.c
@@ -19,7 +19,7 @@ void sched_yield(void)
 	static struct Env *e;
 	//printf("count: %d\n",count);
 	if(count==0) {
-		//while(1) {
+		while(1) {
 			if(e!=NULL){
 				LIST_REMOVE(e, env_sched_link);
 				LIST_INSERT_TAIL(&env_sched_list[1-point], e, env_sched_link);
@@ -27,16 +27,16 @@ void sched_yield(void)
 			if(LIST_EMPTY(&env_sched_list[point])) {
 				point = 1-point;
 			}
-			//e = LIST_FIRST(&env_sched_list[point]);
-			//if(e->env_status==ENV_RUNNABLE) {
-			//	break;
-			//}
-			LIST_FOREACH(e, &env_sched_list[point], env_sched_link) {
-				if(e->env_status==ENV_RUNNABLE) {
-					break;
-				}
+			e = LIST_FIRST(&env_sched_list[point]);
+			if(e->env_status==ENV_RUNNABLE) {
+				break;
 			}
-		
+			//LIST_FOREACH(e, &env_sched_list[point], env_sched_link) {
+			//	if(e->env_status==ENV_RUNNABLE) {
+			//		break;
+			//	}
+			//}
+		}
 		count = e->env_pri;
 	}
 	count--;
