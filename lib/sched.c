09b87b512317c5148ba5129fa24ac23e5e4e8d4e
wzh <2840831011@qq.com>
2022-05-12 14:52:36 +0800

    th

--- lib/sched.c
+++ lib/sched.c
@@ -17,53 +17,34 @@ void sched_yield(void){
 	static int nsch = 0;
     static struct Env *e;
 	int flag=0;
+	int pp;
 	printf("\n");
     if (count == 0 || (e->env_status != ENV_RUNNABLE)) {
 		if(e!=NULL) {
 			if(e->env_pri%2==0){
-				point=nsch+2;
+				pp=nsch+2;
 			}
-			else point=nsch+1;
-			if(point==3) point=0;
-			if(point==4) point=1;
+			else pp=nsch+1;
+			if(pp==3) point=0;
+			if(pp==4) point=1;
 			LIST_INSERT_TAIL(&env_sched_list[point], e, env_sched_link);
 			//printf("insert in %d\n",point);
 		}
-		if(!LIST_EMPTY(&env_sched_list[0])){
-			//printf("finding list 0\n");
-		LIST_FOREACH(e, &env_sched_list[0], env_sched_link) {
-			if(e->env_status==ENV_RUNNABLE) {
-				flag=1;
-				nsch=0;
-				count=e->env_pri;
-				LIST_REMOVE(e, env_sched_link);
-				break;
+		while(flag==0){
+			if(!LIST_EMPTY(&env_sched_list[point])){
+				LIST_FOREACH(e, &env_sched_list[point], env_sched_link) {
+					if(e->env_status==ENV_RUNNABLE) {
+						flag=1;
+						nsch=0;
+						count=(e->env_pri)<<point;
+						LIST_REMOVE(e, env_sched_link);
+						break;
+					}
+				}
 			}
+			point+=1;
+			if(point==3) point=0;
 		}
-		}
-		if(flag==0 && !LIST_EMPTY(&env_sched_list[1])){
-		LIST_FOREACH(e, &env_sched_list[1], env_sched_link) {
-			if(e->env_status==ENV_RUNNABLE) {
-				flag=1;
-				nsch=1;
-				count=(e->env_pri)<<1;
-				LIST_REMOVE(e, env_sched_link);
-				break;
-			}
-		}
-		}
-		if(flag==0 && !LIST_EMPTY(&env_sched_list[2])){
-		LIST_FOREACH(e, &env_sched_list[2], env_sched_link) {
-			if(e->env_status==ENV_RUNNABLE) {
-				flag=1;
-				nsch=2;
-				count=(e->env_pri)<<2;
-				LIST_REMOVE(e, env_sched_link);
-				break;
-			}
-		}
-		}
-		if(flag==0)panic("no runnable env\n");
 	}
 	count--;
     env_run(e);
