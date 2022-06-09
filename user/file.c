09fea698340110e5adf669b9a21e2e2b3c5deef9
wzh <2840831011@qq.com>
2022-06-09 16:47:53 +0800

    th

--- user/file.c
+++ user/file.c
@@ -66,13 +66,13 @@ open(const char *path, int mode)
 	}
 	//fdd=fd;
 	if((O_APPEND & mode)!=0) fd->fd_offset=size;
-	if((O_ALONE & mode)!=0) {
-		syscall_mem_map(0, fd, 0, fd, PTE_V | PTE_R);
+	//if((O_ALONE & mode)!=0) {
+	//	syscall_mem_map(0, fd, 0, fd, PTE_V | PTE_R);
 		//syscall_mem_alloc(0, fd, PTE_V | PTE_R);
-		for(i=va;i<va+size;i+=BY2PG) {
-			syscall_mem_map(0, i, 0, i, PTE_V | PTE_R);
-		}
-	}
+	//	for(i=va;i<va+size;i+=BY2PG) {
+	//		syscall_mem_map(0, i, 0, i, PTE_V | PTE_R);
+	//	}
+	//}
 	// Step 5: Return the number of file descriptor.
 	return fd2num(fd);
 
