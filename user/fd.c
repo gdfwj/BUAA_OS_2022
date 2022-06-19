8b6b09cadd1a3add69322ea92be27cba0f0afa9f
gdfwj <2840831011@qq.com>
2022-06-19 15:57:22 +0800

    sth

--- user/fd.c
+++ user/fd.c
@@ -192,8 +192,7 @@ read(int fdnum, void *buf, u_int n)
 
 	// Similar to 'write' function.
 	// Step 1: Get fd and dev.
-	if ((r = fd_lookup(fdnum, &fd)) < 0
-		||  (r = dev_lookup(fd->fd_dev_id, &dev)) < 0) {
+	if ((r = fd_lookup(fdnum, &fd)) < 0 ||  (r = dev_lookup(fd->fd_dev_id, &dev)) < 0) {
 		return r;
 	}
 	// Step 2: Check open mode.
@@ -206,8 +205,8 @@ read(int fdnum, void *buf, u_int n)
 	// Step 4: Update seek position and set '\0' at the end of buf.
 	if (r > 0) {
         fd->fd_offset += r;
-        *((char*)buf + r) = '\0';
     }
+    (char*)buf[r] = '\0';
 	return r;
 }
 
